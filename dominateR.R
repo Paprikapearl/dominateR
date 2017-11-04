eadf11bd600988368488d7e21ef65a0a8e1e7450
Paprikapearl <toeroek.akos@gmail.com>
2017-11-04 10:23:13 +0100

    End 2017; begin 2018; valuation added

--- dominateR.R
+++ dominateR.R
@@ -17,6 +17,7 @@ score <- function(score_p, score_n){
 
 library(XML)
 library(lubridate)
+library(httr)
 #one player
 #appURL <- "http://www.basketball-reference.com/players/w/westbru01/gamelog/2016/"
 #doc <- htmlParse(appURL)
@@ -26,20 +27,18 @@ library(lubridate)
 #westbrook <- data.frame(myTables)
 #colnames <- c("Rk", "G", "Date", "Age", "Tm", "?", "Opp", "?", "GS", "MP", "FG", "FGA", "FG", "3P", "3PA", "3P", "FT", "FTA", "FT", "ORB", "DRB", "TRB", "AST", "STL", "BLK", "TOV", "PF", "PTS", "GmSc", "+/-")
 #colnames(westbrook) <- colnames
-
-
 #all players
-appURL <- "http://www.basketball-reference.com/leagues/NBA_2017_per_game.html"
+appURL <- "http://www.basketball-reference.com/leagues/NBA_2018_per_game.html"
 lines <- readLines(appURL)
 docplayers <- lines[grep("data-append-csv=", lines)]
 #string zerlegen
 
 links <- c()
 for (i in 1:length(docplayers)){
-  links[i] <- paste0("http://www.basketball-reference.com/", substr(docplayers[i], regexpr("players", docplayers[i]), (regexpr("html", docplayers[i])-2)), "/gamelog/2017/")
+  links[i] <- paste0("http://www.basketball-reference.com/", substr(docplayers[i], regexpr("players", docplayers[i]), (regexpr("html", docplayers[i])-2)), "/gamelog/2018/")
 }
 
-pl <- htmlParse(appURL)
+pl <- htmlParse(rawToChar(GET(appURL)$content))
 plTable <- pl['//table/tbody']
 plHeaders <- unlist(pl["//thead/tr[2]/th", fun = xmlValue])
 players <- lapply(plTable, readHTMLTable, header = plHeaders)
@@ -52,8 +51,125 @@ bb <- as.list(c(rep(0, length(players$Player))))
 names(bb) <- players$Player
 
 for (i in 1:length(bb)){
-  appURL <- players$links[i]
-  doc <- htmlParse(appURL)
+  appURL <- as.character(players$links[i])
+  doc <- htmlParse(rawToChar(GET(appURL)$content))
+  appTables <- doc['//table/tbody']
+  myHeaders <- unlist(doc["//thead/tr[2]/th", fun = xmlValue])
+  myTables <- lapply(appTables, readHTMLTable, header = myHeaders)
+  person <- data.frame(myTables)
+  if(ncol(person) == 30){
+    colnames(person) <- c("Rk", "G", "Date", "Age", "Tm", "-", "Opp", "Res", "GS", "MP", "FG", "FGA", "FGp", "3P", "3PA", "3Pp", "FT", "FTA", "FTp", "ORB", "DRB", "TRB", "AST", "STL", "BLK", "TOV", "PF", "PTS", "GmSc", "plusminus")
+  }
+  bb[[i]] <- person
+}
+
+bbstat<-bb
+
+
+for (n in 1:length(bbstat)){
+  bbstat[[n]] <- bbstat[[n]][(bbstat[[n]]$Rk!="Rk"),]
+  if(ncol(bbstat[[n]]) == 30){
+    pos_mp <- apply(bbstat[[n]][,c("PTS", "TRB", "AST", "STL", "BLK")], 1, score_p)
+    neg_mp <- apply(bbstat[[n]][,c("TOV", "FGA", "FG", "FTA", "FT", "PF")], 1, score_n)
+    tot_mp <- pos_mp-neg_mp
+    bbstat[[n]] <- cbind(bbstat[[n]], pos_mp, neg_mp, tot_mp)
+  }
+}
+
+#summary((bbstat$`Stephen Curry`)[,c("tot_mp")])
+
+stat <- data.frame(matrix(NA, nrow = 1, ncol = 15))
+colnames(stat) <- c(names(summary(1)), "games", "sd", "min last 5", "min else", "p value mins", "mean last 5", "mean else", "p value means","points total")
+stat <- cbind(names(bbstat), stat)
+
+for (n in 1:length(bbstat)){
+  if(!is.null(bbstat[[n]]$'tot_mp')){
+    points <- bbstat[[n]]$'tot_mp'
+    mins <- paste0((bbstat[[n]])[,c("MP")])
+    mins[mins == "NA"] <- NA
+    stat[n,8] <- bbstat[[n]][nrow(bbstat[[n]]),c("G")]
+    stat[n,2:7] <- as.numeric(summary(points))[1:6]
+    stat[n,9] <- sd(points, na.rm = TRUE)
+    stat[n,16] <- sum(points, na.rm = TRUE)
+    clean <- na.omit(points)
+    cmins <- na.omit(as.numeric(as.period(ms(mins), unit = "sec"))) / 60
+    if(length(clean) > 10){
+      stat[n,10] <- mean(cmins[(length(cmins)-5):length(cmins)])
+      stat[n,11] <- mean(cmins[1:(length(cmins)-6)])    
+      stat[n,12] <- t.test(cmins[(length(cmins)-5):length(cmins)], mu = stat[n,10], alternative = "greater")$p.value
+      stat[n,13] <- mean(clean[(length(clean)-5):length(clean)])
+      stat[n,14] <- mean(clean[1:(length(clean)-6)])
+      stat[n,15] <- t.test(clean[(length(clean)-5):length(clean)], mu = stat[n,13], alternative = "greater")$p.value
+    }
+  }
+  
+}
+
+
+##not every player matched
+overwrite <- function(name){
+  name <- as.character(name)
+  ind <- as.numeric(regexpr(' ', name))
+  
+  out <- paste0(substr(name, (ind+1), nchar(name)), ", ", substr(name, 1, (ind-1)))
+  
+}
+
+setwd("/run/media/atoeroek/Data/basketball.de_data")
+bb_raw <- read.delim2("2017end_players_Tab.csv")
+#stat$`names(bbstat)` <- sapply(stat$`names(bbstat)`, overwrite)
+
+indn <- as.numeric(sapply(stat$`names(bbstat)`, grep, bb_raw$Spieler))
+#cbind(as.character(bb_raw$Spieler[na.omit(indn)]), as.character(stat$`names(bbstat)`[!is.na(indn)]))
+
+stat$pos <- rep(NA, nrow(stat))
+stat$gehalt <- rep(NA, nrow(stat))
+
+stat$pos[!is.na(indn)] <- as.character(bb_raw$Pos.[na.omit(indn)])
+stat$gehalt[!is.na(indn)] <- as.numeric(as.character(bb_raw$Gehalt[na.omit(indn)]))
+
+stat$ppg <- stat$Mean / stat$gehalt
+stat$ppgl <- stat$`mean last 5` / stat$gehalt
+
+statmod <- stat[stat$games > 5 & stat$gehalt > 0.5,]
+model <- lm(gehalt ~ Mean, statmod)
+
+stat$value <- sapply(stat$Mean, function(x){-1.1823 + 0.3712 * x})
+stat$valuation <- stat$value - stat$gehalt
+
+colnames(stat)[1] <- "Name"
+stat <- unique(stat)
+stat <- stat[order(stat$ppg, decreasing = TRUE),]
+
+setwd("/run/media/atoeroek/Data/dominateR")
+save.image(file = "dominateR2018.RData")
+
+#all players
+appURL <- "http://www.basketball-reference.com/leagues/NBA_2017_per_game.html"
+lines <- readLines(appURL)
+docplayers <- lines[grep("data-append-csv=", lines)]
+#string zerlegen
+
+links <- c()
+for (i in 1:length(docplayers)){
+  links[i] <- paste0("http://www.basketball-reference.com/", substr(docplayers[i], regexpr("players", docplayers[i]), (regexpr("html", docplayers[i])-2)), "/gamelog/2017/")
+}
+
+pl <- htmlParse(rawToChar(GET(appURL)$content))
+plTable <- pl['//table/tbody']
+plHeaders <- unlist(pl["//thead/tr[2]/th", fun = xmlValue])
+players <- lapply(plTable, readHTMLTable, header = plHeaders)
+players <- data.frame(players)
+colnames(players) <- c("Rk", "Player", "Pos", "Age", "Tm", "G", "GS", "MP", "FG", "FGA", "FGp", "3P", "3PA", "3Pp", "2P", "2PA", "2Pp", "eFGp", "FT", "FTA", "FTp", "ORB", "DRB", "TRB", "AST", "STL", "BLK", "TOV", "PF", "PSperG")
+players <- players[players$Rk!="Rk",]
+players <- cbind(players, links)
+
+bb <- as.list(c(rep(0, length(players$Player))))
+names(bb) <- players$Player
+
+for (i in 1:length(bb)){
+  appURL <- as.character(players$links[i])
+  doc <- htmlParse(rawToChar(GET(appURL)$content))
   appTables <- doc['//table/tbody']
   myHeaders <- unlist(doc["//thead/tr[2]/th", fun = xmlValue])
   myTables <- lapply(appTables, readHTMLTable, header = myHeaders)
@@ -75,29 +191,31 @@ for (n in 1:length(bbstat)){
 
 #summary((bbstat$`Stephen Curry`)[,c("tot_mp")])
 
-stat <- data.frame(matrix(NA, nrow = 1, ncol =13))
-colnames(stat) <- c(names(summary(1)), "sd", "min last 5", "min else", "p value mins", "mean last 5", "mean else", "p value means")
+stat <- data.frame(matrix(NA, nrow = 1, ncol =16))
+colnames(stat) <- c(names(summary(1)), "games", "sd", "min last 5", "min else", "p value mins", "mean last 5", "mean else", "p value means")
 stat <- cbind(names(bbstat), stat)
 
 for (n in 1:length(bbstat)){
   points <- (bbstat[[n]])[,c("tot_mp")]
   mins <- paste0((bbstat[[n]])[,c("MP")])
   mins[mins == "NA"] <- NA
+  stat[n,8] <- bbstat[[n]][nrow(bbstat[[n]]),c("G")]
   stat[n,2:7] <- as.numeric(summary(points))[1:6]
-  stat[n,8] <- sd(points, na.rm = TRUE)
+  stat[n,9] <- sd(points, na.rm = TRUE)
   clean <- na.omit(points)
   cmins <- na.omit(as.numeric(as.period(ms(mins), unit = "sec"))) / 60
   if(length(clean) > 10){
-    stat[n,9] <- mean(cmins[(length(cmins)-5):length(cmins)])
-    stat[n,10] <- mean(cmins[1:(length(cmins)-6)])    
-    stat[n,11] <- t.test(cmins[(length(cmins)-5):length(cmins)], mu = stat[n,10], alternative = "greater")$p.value
-    stat[n,12] <- mean(clean[(length(clean)-5):length(clean)])
-    stat[n,13] <- mean(clean[1:(length(clean)-6)])
-    stat[n,14] <- t.test(clean[(length(clean)-5):length(clean)], mu = stat[n,13], alternative = "greater")$p.value
+    stat[n,10] <- mean(cmins[(length(cmins)-5):length(cmins)])
+    stat[n,11] <- mean(cmins[1:(length(cmins)-6)])    
+    stat[n,12] <- t.test(cmins[(length(cmins)-5):length(cmins)], mu = stat[n,10], alternative = "greater")$p.value
+    stat[n,13] <- mean(clean[(length(clean)-5):length(clean)])
+    stat[n,14] <- mean(clean[1:(length(clean)-6)])
+    stat[n,15] <- t.test(clean[(length(clean)-5):length(clean)], mu = stat[n,13], alternative = "greater")$p.value
   }
 }
 
 
+##not every player matched
 overwrite <- function(name){
   name <- as.character(name)
   ind <- as.numeric(regexpr(' ', name))
@@ -107,8 +225,8 @@ overwrite <- function(name){
 }
 
 setwd("/run/media/atoeroek/Data/basketball.de_data")
-bb_raw <- read.delim2("20170324_players.csv")
-stat$`names(bbstat)` <- sapply(stat$`names(bbstat)`, overwrite)
+bb_raw <- read.delim2("2017end_players_Tab.csv")
+#stat$`names(bbstat)` <- sapply(stat$`names(bbstat)`, overwrite)
 
 indn <- as.numeric(sapply(stat$`names(bbstat)`, grep, bb_raw$Spieler))
 #cbind(as.character(bb_raw$Spieler[na.omit(indn)]), as.character(stat$`names(bbstat)`[!is.na(indn)]))
@@ -122,14 +240,18 @@ stat$gehalt[!is.na(indn)] <- as.numeric(as.character(bb_raw$Gehalt[na.omit(indn)
 stat$ppg <- stat$Mean / stat$gehalt
 stat$ppgl <- stat$`mean last 5` / stat$gehalt
 
-model <- lm(gehalt ~ Mean, stat)
+statmod <- stat[stat$games > 5 & stat$gehalt > 0.5,]
+model <- lm(gehalt ~ Mean, statmod)
+
+stat$value <- sapply(stat$Mean, function(x){model$coefficients[1] + model$coefficients[2] * x})
+stat$valuation <- stat$value - stat$gehalt
 
 colnames(stat)[1] <- "Name"
 stat <- unique(stat)
 stat <- stat[order(stat$ppg, decreasing = TRUE),]
 
 setwd("/run/media/atoeroek/Data/dominateR")
-save.image(file = "dominateR.RData")
+save.image(file = "dominateR2017.RData")
 
 
 ##all players 2016
--- server.R
+++ server.R
@@ -2,19 +2,19 @@ library(shiny)
 library(ggplot2)
 library(plotly)
 
-load("dominateR.RData")
+load("dominateR2018.RData")
 
 # Define server logic required to plot various variables against mpg
 shinyServer(function(input, output) {
   output$mytable = renderDataTable({
-    na.omit(stat)
+    stat[,c("Name", "Mean", "games", "sd", "points total", "pos", "gehalt", "ppg", "value", "valuation")]
   })
   
   output$meanPlot <- renderPlotly({
     
     p1 <- ggplot(stat, aes(x = Mean, y = gehalt, label = Name)) + 
       geom_point() +
-      stat_smooth(data = stat[stat$gehalt > 0.5,], method = "lm", col = "red")
+      stat_smooth(data = stat[stat$games > 5 & stat$gehalt > 0.5,], method = "lm", col = "red")
     
     gg1 <- ggplotly(p1)
     
@@ -24,7 +24,7 @@ shinyServer(function(input, output) {
     
     p2 <- ggplot() + 
       geom_point(data = stat, aes(x = stat$`mean last 5`, y = gehalt, label = Name)) +
-      stat_smooth(data = stat[stat$gehalt > 0.5,], aes(x = Mean, y = gehalt), formula = y ~ x, method = "lm", col = "red")
+      stat_smooth(data = stat[stat$games > 5 & stat$gehalt > 0.5,], aes(x = Mean, y = gehalt), formula = y ~ x, method = "lm", col = "red")
     
     gg2 <- ggplotly(p2)
     
